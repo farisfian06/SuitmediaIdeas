094715a6702bcac7bab103f885c1089fd9349f72
farisfian06 <farisfian06@gmail.com>
2025-07-13 22:05:29 +0700

    feat: update app branding and styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./style.css";
 import Routes from "./routes/Routes.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
