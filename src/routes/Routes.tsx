57869a6f8b8f82be8883b4117c79c962787bf19f
farisfian06 <farisfian06@gmail.com>
2025-07-13 22:09:46 +0700

    feat(routing): add page routes with layout wrapper

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,78 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "../App";
+import HomePage from "../pages/HomePage";
+import PageLayout from "../layout/PageLayout";
+import NotFound from "../pages/NotFound";
+import IdeasPage from "../pages/IdeasPage";
+import WorkPage from "../pages/WorkPage";
+import AboutPage from "../pages/AboutPage";
+import ServicesPage from "../pages/ServicesPage";
+import CareersPage from "../pages/CareersPage";
+import ContactPage from "../pages/ContactPage";
 
 const createRouter = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: (
+      <PageLayout>
+        <HomePage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "/work",
+    element: (
+      <PageLayout>
+        <WorkPage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <PageLayout>
+        <AboutPage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "/services",
+    element: (
+      <PageLayout>
+        <ServicesPage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "/ideas",
+    element: (
+      <PageLayout>
+        <IdeasPage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "/careers",
+    element: (
+      <PageLayout>
+        <CareersPage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "/contact",
+    element: (
+      <PageLayout>
+        <ContactPage />
+      </PageLayout>
+    ),
+  },
+  {
+    path: "*",
+    element: (
+      <PageLayout>
+        <NotFound />
+      </PageLayout>
+    ),
   },
 ]);
 
